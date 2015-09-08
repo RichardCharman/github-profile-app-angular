9b42dea9bc52ad4bc81818fd7ec28a469ad9ed35
RichardCharman <richardcharman@live.com>
2015-09-08 13:11:18 +0000

    Completed protractor pill

--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -1,7 +1,27 @@
-describe('Github Profile finder', function() {
-  it('has a title', function() {
-    browser.get('http://localhost:0000');
+describe('GitHub profile finder', function() {
 
+  var searchBox = element(by.model('searchTerm'))
+  var searchButton = element(by.className('btn'))
+
+  beforeEach(function() {
+    browser.get('http://localhost:8080');
+  })
+
+  it('has a title', function() {
     expect(browser.getTitle()).toEqual('Github user search');
+  });
+
+  it('finds profiles', function() {
+    //Complete this test using the variables above!
+    expect(element(by.binding('user.login')).getText()).toEqual('spike01');
+  });
+
+  it('finds profiles', function() {
+
+    searchBox.sendKeys('spike');
+    searchButton.click();
+
+    var profiles = element.all(by.repeater('user in searchCtrl.searchResult.items'));
+    expect(profiles.last().getText()).toEqual('spike');
   });
 });
