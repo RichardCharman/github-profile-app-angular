990d0b1154666b893bba5db1f7a4ac6bb8e9ed45
RichardCharman <richardcharman@live.com>
2015-09-08 18:56:31 +0000

    Heroku 6

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -6,10 +6,10 @@ module.exports = function(config) {
       frameworks: ['jasmine'],
 
       files: [
-          'bower_components/angular/angular.js',
-          'bower_components/angular-route/angular-route.js',
-          'bower_components/angular-resource/angular-resource.js',
-          'bower_components/angular-mocks/angular-mocks.js',
+          'public/bower_components/angular/angular.js',
+          'public/bower_components/angular-route/angular-route.js',
+          'public/bower_components/angular-resource/angular-resource.js',
+          'public/bower_components/angular-mocks/angular-mocks.js',
           'js/**/*.js',
           'test/**/*.spec.js'
       ],
